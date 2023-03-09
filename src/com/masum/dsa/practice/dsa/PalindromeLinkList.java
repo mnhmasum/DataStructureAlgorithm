854ea1f534a796161616342b7fcc823bb6e29bae
nazmul <nhasan@ael-bd.com>
2023-03-09 21:21:31 +0600

    Remove unused code from PalindromeLinkList.java

--- src/com/masum/dsa/practice/dsa/PalindromeLinkList.java
+++ src/com/masum/dsa/practice/dsa/PalindromeLinkList.java
@@ -1,9 +1,6 @@
 package com.masum.dsa.practice.dsa;
 
-import java.util.LinkedList;
-import java.util.PriorityQueue;
 import java.util.Stack;
-import sun.misc.Queue;
 
 /**
  *
@@ -21,34 +18,22 @@ public class PalindromeLinkList {
         Node node3 = new Node();
         node3.data = 1;
         node2.next = node3;
-        
+
         Node auxHead = head;
-   
+
         Stack<Integer> queue = new Stack();
         while (auxHead != null) {
             queue.add(auxHead.data);
             auxHead = auxHead.next;
         }
-        
-//        Node p = null, q = null, r = null;
-//        r = head;
-//        
-//        while (r != null) {
-//            p = q;
-//            q = r;
-//            r = r.next;
-//            q.next = p;
-//        }
-//        
-//        
-//        head = q;
+
         while (head != null) {
             System.out.println(head.data);
-            if(head.data != queue.pop()) {
+            if (head.data != queue.pop()) {
                 System.out.println("false");
                 break;
             }
-           
+
             head = head.next;
         }
 
